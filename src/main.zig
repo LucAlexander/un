f3c8ede70b76870d5e237b4634bf9f6b16ed2aea
Luc Alexander <lucskyalex@gmail.com>
2025-12-08 15:44:56 +0100

    strict normalizer without parsing

--- src/main.zig
+++ src/main.zig
@@ -527,8 +527,134 @@ const Program = struct {
 	}
 
 	pub fn normalize(self: *Program, normalized: *Buffer(Expr), expr: *Expr) ?*Expr {
-		//TODO return flattened expression, the return of the block
-		//TODO add subsequent instructions to normalized buffer
+		for (expr.list.items[..expr.list.items.len-1]) |inst| {
+			if (inst == .atom){
+				return null;
+			}
+			if (inst.list.items[0].* == .list){
+				return null;
+			}
+			switch (inst.list.items[0].atom.tag){
+				.REG, .LABEL => {
+					if (inst.list.items.len != 2){
+						return null;
+					}
+					if (self.expect_token(normalized, inst.list.items[1])){
+						normalized.append(inst)
+							catch unreachable;
+						continue;
+					}
+					return null;
+				},
+				.MOV => {
+					if (inst.list.items.len != 3){
+						return null;
+					}
+					if (self.expect_register(normalized, inst.list.items[1])){
+						if (self.expect_dregister(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+						if (self.expect_register(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+						if (self.expect_literal(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+					}
+					if (self.expect_dregister(normalized, inst.list.items[1])){
+						if (self.expect_dregister(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+						if (self.expect_register(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+						if (self.expect_literal(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+					}
+					return null;
+				},
+				.ADD, .SUB, .MUL, .DIV, .MOD, .UADD, .USUB, .UMUL, .UDIV, .UMOD, .SHR, ..SHL, .AND, .OR, .XOR => {
+					if (inst.list.items.len != 3){
+						return null;
+					}
+					if (self.expect_register(normalized, inst.list.items[1])){
+						if (self.expect_alu_arg(normalized, inst.list.items[2])){
+							if (self.expect_alu_arg(normalized, inst.list.items[3])){
+								normalized.append(inst)
+									catch unreachable;
+								continue;
+							}
+						}
+					}
+					return null;
+				},
+				.NOT, .COM, .CMP=> {
+					if (inst.list.items.len != 2){
+						return null;
+					}
+					if (self.expect_register(normalized, inst.list.items[1])){
+						if (self.expect_alu_arg(normalized, inst.list.items[2])){
+							normalized.append(inst)
+								catch unreachable;
+							continue;
+						}
+					}
+					return null;
+				},
+				.JMP, JEQ, JNE, JGT, JGE, JLT, JLE, CALL => {
+					if (inst.list.items.len != 2){
+						return null;
+					}
+					if (self.expect_literal(normalized, inst.list.items[1])){
+						normalized.append(inst)
+							catch unreachable;
+						continue;
+					}
+					return null;
+				},
+				.RET => {
+					if (inst.list.items.len != 2){
+						return null;
+					}
+					if (self.expect_alu_arg(normalized, inst.list.items[1])){
+						normalized.append(inst)
+							catch unreachable;
+						continue;
+					}
+					return null;
+				},
+				.PSH, POP => {
+					if (inst.list.items.len != 2){
+						return null;
+					}
+					if (self.expect_register(normalized, inst.list.items[1])){
+						normalized.append(inst)
+							catch unreachable;
+						continue;
+					}
+					return null;
+				},
+				.INT => {
+					normalized.append(inst)
+						catch unreachable;
+					continue;
+				}
+			}
+		}
+		return expr.list.items[expr.list.items.len-1];
 	}
 
 	pub fn parse_ir(self: *Program, expr: *Expr) ?Buffer(ir.Instruction) {
