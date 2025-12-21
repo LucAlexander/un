2e9d36613016ddd948f8e83ab30718e21c85618c
Luc Alexander <lucskyalex@gmail.com>
2025-12-21 19:42:59 +0100

    program termination on vm

--- src/main.zig
+++ src/main.zig
@@ -1975,6 +1975,23 @@ const Program = struct {
 		};
 		context.await_cores();
 		context.deinit();
+		if (std.mem.eql(u8, vm_target.text, "vm")){
+			const return_value = vm.?.cores[0].reg[1];
+			const loc = self.mem.create(Expr)
+				catch unreachable;
+			const buf = self.mem.alloc(u8, 20)
+				catch unreachable;
+			const slice = std.fmt.bufPrint(buf, "{}", .{return_value})
+				catch unreachable;
+			loc.* = Expr{
+				.atom = Token{
+					.pos=0,
+					.tag=.NUM,
+					.text=slice
+				}
+			};
+			return loc;
+		}
 		const return_address = vm.?.cores[0].reg[3];
 		return self.lift_reif(vm.?, repr.reif, return_address);
 	}
